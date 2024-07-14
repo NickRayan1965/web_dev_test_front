c7960192740f6a7d008389527df178eea1487676
NickRayan <nickcerron@gmail.com>
2024-07-14 06:09:52 -0500

    referencia innecesaria quitada

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { UserForm } from './components/user/UserForm';
 import { LoginForm } from './components/auth/Login';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import PrivateRoute from './auth/privateRoute.jsx';
-import {} from 'react-router-dom';
 import PanelTask from './components/task/PanelTask';
 import PanelUser from './components/user/PanelUser';
 export default function App() {
